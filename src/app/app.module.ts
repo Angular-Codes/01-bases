bf7d7cde624ade92578174fece425404afc584e8
Kevin cuadros <kevindavidcuadros@gmail.com>
2022-10-08 20:46:53 -0500

    heroe component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // declaro el componente para usarlo
+    CounterComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule
